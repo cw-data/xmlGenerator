73f66701e0751029e94013cd5e0a869d188f7f22
cw-data <charles.a.wainright@gmail.com>
2023-02-12 10:35:08 -0500

    created minimal workflow for testing

--- main.R
+++ main.R
@@ -1,3 +1,6 @@
-# placeholder
+# a minimal workflow
 library(xmlGenerator)
+
 xmlGenerator::load_example_data()
+xmlGenerator::loadData(example_data)
+xmlGenerator::buildXML(record_list, write = FALSE)
