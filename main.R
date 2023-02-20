7dcff39fae9b5b8ee73002e7dd5b06cc4e7b4686
cw-data <charles.a.wainright@gmail.com>
2023-02-20 11:19:33 -0500

    added documentation

--- main.R
+++ main.R
@@ -9,19 +9,25 @@ library(xmlGenerator)
 
 
 #--------------------------------
-# A minimal reproducible workflow
+# A minimal reproducible workflow that's included with library(xmlGenerator)
+# Purpose: include in the package everything a user needs to "see what's going on" without providing their own data
 #--------------------------------
 
 # package includes example data so a user can see what the required data format is
-# xmlGenerator::load_example_data()
+xmlGenerator::load_example_data()
+xmlGenerator::build_xml(record_list)
 
-# step 1: assign the filepath to your forms data as a character string in variable `forms_spreadsheet`
-forms_spreadsheet <- "data-raw/testdata.xlsx"
+#--------------------------------
+# An example "real" workflow
+# Purpose: mock up the exact workflow a user needs to follow to use library(xmlGenerator) to make xml
+#--------------------------------
+# step 1: assign the filepath to your forms data as a character string
+FORMS_SPREADSHEET <- "data-raw/testdata.xlsx"
 # step 2: run loadData()
 # this function validates your input data for filetype, colnames, reference types
 # then it parses your data into a list (an instance of class record_list)
-xmlGenerator::loadData(forms_spreadsheet = forms_spreadsheet)
+xmlGenerator::load_data(spreadsheet_path = FORMS_SPREADSHEET)
 # step 3: run buildXML()
 # this function builds xml out of your `record_list` object
 # "write = TRUE" will bring up a dialog for the user to choose where to save the xml output
-xmlGenerator::buildXML(record_list, write = FALSE)
+xmlGenerator::build_xml(record_list, write = FALSE)
