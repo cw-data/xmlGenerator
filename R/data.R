79ee725b599181e8d319f16685f4e9a74e1fc9b2
cw-data <charles.a.wainright@gmail.com>
2023-02-20 12:36:42 -0500

    added documentation

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Example of xmlGenerator-acceptable data
+#'
+#' A dataset containing example MS Forms data in the the column name and column order requuired by xmlGenerator.
+#'  The use \code{colnames(example_data)} to extract column names and column order.
+#'
+#' @format A data frame with 19 rows and 158 variables.
+"example_data"
